c517e05e05e0e4e4abd3a36838b0cdafeddc2b63
kubavation <37585627+kubavation@users.noreply.github.com>
2023-09-14 17:31:41 +0200

    init

---
+++ src/test/java/com/durys/jakub/engagementservice/EngagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durys.jakub.engagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
