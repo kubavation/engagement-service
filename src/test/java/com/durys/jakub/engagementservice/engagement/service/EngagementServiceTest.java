3c9b9ef077220fd187b88b323a07445e70898692
kubavation <37585627+kubavation@users.noreply.github.com>
2023-09-14 20:23:37 +0200

    grpc tests

--- src/test/java/com/durys/jakub/engagementservice/engagement/service/EngagementServiceTest.java
+++ src/test/java/com/durys/jakub/engagementservice/engagement/service/EngagementServiceTest.java
@@ -1,7 +1,10 @@
 package com.durys.jakub.engagementservice.engagement.service;
 
+import com.durys.jakub.engagementservice.model.EngagementProto;
 import com.durys.jakub.engagementservice.model.EngagementServiceGrpc;
+import com.google.protobuf.Int32Value;
 import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
@@ -10,13 +13,27 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(properties = {
         "grpc.server.inProcessName=test",
         "grpc.server.port=-1",
-        "grpc.client.inProcess.address=in-process:test"
+        "grpc.client.testClient.address=in-process:test"
 })
 @DirtiesContext
 class EngagementServiceTest {
 
 
-    @GrpcClient("inProcess")
+    @GrpcClient("testClient")
     EngagementServiceGrpc.EngagementServiceBlockingStub service;
+
+    @Test
+    void shouldFindAllEngagementsOfUser() {
+        EngagementProto.Engagements engagements = service.findAllByUser(Int32Value.newBuilder().setValue(11).build());
+
+        assertNotNull(engagements);
+        assertTrue(engagements.getEngagementList().isEmpty());
+    }
+
+    @Test
+    void shouldFindEngagementById() {
+        EngagementProto.Engagement engagement = service.findById(Int32Value.newBuilder().setValue(11).build());
+        assertNotNull(engagement);
+    }
 
 }
